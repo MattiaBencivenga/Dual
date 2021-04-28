663e292df00d07a8a9fb2f579e5b6d5de7485306
Mattia Bencivenga <mattia.bencivenga@studenti.polito.it>
2021-04-28 12:26:36 +0200

    Imported assets

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,14 +1,13 @@
 using System;
 using UnityEngine;
 
-#pragma warning disable 618
 namespace UnityStandardAssets.Utility
 {
     public class SimpleActivatorMenu : MonoBehaviour
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public UnityEngine.UI.Text camSwitchButton;
+        public GUIText camSwitchButton;
         public GameObject[] objects;
 
 
